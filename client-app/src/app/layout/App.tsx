0d87d9f4f33e1499a1348d0489eaece99b3d445c
Sundar Urs <sundarkurs@gmail.com>
2020-05-27 23:08:47 +0530

    React - UI, CRUD at client side, Form and props to child components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,40 +1,74 @@
-import React, { useState, useEffect, Fragment } from 'react'
-import { Header, Icon, List, Container } from 'semantic-ui-react'
+import React, { useState, useEffect, Fragment } from 'react';
+import { Container } from 'semantic-ui-react';
 import axios from 'axios';
 import { IActivity } from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+  const [editMode, setEditMode] = useState(false);
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  }
+
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity])
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter(a => a.id !== id)])
+  }
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter(a => a.id === id)[0]);
+    setEditMode(false);
+  };
 
   useEffect(() => {
-
-    axios.get<IActivity[]>('http://localhost:5000/api/activities')
-      .then((response) => {
-        setActivities(response.data);
-      })
-
-  }, [])
+    axios
+      .get<IActivity[]>('http://localhost:5000/api/activities')
+      .then(response => {
+        let activities: IActivity[] = [];
+        response.data.forEach(activity => {
+          activity.date = activity.date.split('.')[0]
+          activities.push(activity);
+        })
+        setActivities(activities);
+      });
+  }, []);
 
   return (
     <Fragment>
-      <NavBar></NavBar>
-
-      <Header as='h2'>
-        <Icon name='plug' />
-        <Header.Content>Reactivities</Header.Content>
-      </Header>
-
+      <NavBar openCreateForm={handleOpenCreateForm} />
       <Container style={{ marginTop: '7em' }}>
-        <List>
-          {activities.map((activitie) => (
-            <List.Item key={activitie.id}>{activitie.title}</List.Item>
-          ))}
-        </List>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
       </Container>
-
     </Fragment>
-  )
-}
+  );
+};
 
 export default App;
